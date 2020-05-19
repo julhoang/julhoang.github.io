bb762a61dc8247bdeb1c9b90ea964cc0c161e9ec
julhoang <64108232+julhoang@users.noreply.github.com>
2020-05-18 23:40:30 -0700

    Pong 2

--- exercise9/pong.js
+++ exercise9/pong.js
@@ -6,6 +6,29 @@ var positionOfPaddle1 = document.getElementById("paddle1").offsetTop;
 var speedOfPaddle2 = 0;
 var positionOfPaddle2 = document.getElementById("paddle2").offsetTop;
 
+const paddleHeight = document.getElementById("paddle1").offsetHeight;
+const paddleWidth = document.getElementById("paddle1").offsetWidth;
+
+var gameboardHeight = document.getElementById("gameBoard").offsetHeight;
+var gameBoardWidth = document.getElementById("gameBoard").offsetWidth;
+var ballHeight = document.getElementById("ball").offsetHeight;
+
+const startTopPositionOfBall = document.getElementById("ball").offsetTop;
+const startLeftPositionOfBall = document.getElementById("ball").offsetLeft;
+
+var topPositionofBall = startTopPositionOfBall;
+var leftPositionOfBall = startLeftPositionOfBall;
+var topSpeedOfBall = 0;
+var leftSpeedOfBall = 0;
+
+var score1 = 1;
+var score2 = 1;
+
+// start ball motion
+window.addEventListener('load', function() {
+	startBall();
+})
+
 // Move paddles
 document.addEventListener('keydown', function(e) {
 	//console.log("key down " + e.keyCode);
@@ -26,7 +49,6 @@ document.addEventListener('keydown', function(e) {
 	} // if
 
 
-	show();
 
 });
 
@@ -41,20 +63,35 @@ document.addEventListener('keyup', function(e) {
 		speedOfPaddle2 = 0;
 	} // if
 
-	show();
+	
 });
 
+function startBall() {
+	let direction = 1;
+	topPositionofBall = startTopPositionOfBall;
+	leftPositionOfBall = startLeftPositionOfBall;
+
+	// 50% chance of starting right or left
+	if (Math.random() < 0.5) {
+		direction = 1;
+	} else {
+		direction = -1;
+	} // if
+
+	topSpeedOfBall = Math.random() * 2 + 3; // 3-4.9999
+	leftSpeedOfBall = direction * (Math.random() * 2 + 3);
+
+
+} // startBall
 
 // update locations of paddles and ball
-function show() {
-	let paddleHeight = document.getElementById("paddle1").offsetHeight;
-	let gameboardHeight = document.getElementById("gameBoard").offsetHeight;
+window.setInterval( function show() {
 
+	// update positions of elements
 	positionOfPaddle1 += speedOfPaddle1;
-	document.getElementById("paddle1").style.top = positionOfPaddle1 + "px";
-
 	positionOfPaddle2 += speedOfPaddle2;
-	document.getElementById("paddle2").style.top = positionOfPaddle2 + "px";
+	topPositionofBall += topSpeedOfBall;
+	leftPositionOfBall += leftSpeedOfBall;
 
 	// stop paddle from leaving the top of the gameboard
 	if (positionOfPaddle1 <= 0) {
@@ -75,8 +112,44 @@ function show() {
 		positionOfPaddle2 = gameboardHeight - paddleHeight;
 	} // if
 
+	// if ball hits top or bottom of gameboard, change direction
+	if (topPositionofBall <= 0 || topPositionofBall >= gameboardHeight - ballHeight) {
+		topSpeedOfBall *= -1;
 
-} // show
+	} // if
 
+	// ball on left edge of gameBoard
+	if (leftPositionOfBall <= paddleWidth) {
+		
+		// if ball hits left paddle, change direction
+		if (topPositionofBall >= positionOfPaddle1 && topPositionofBall < positionOfPaddle1 + paddleHeight) {
+			leftSpeedOfBall *= -1;
+		} else {
+			startBall();
+			document.getElementById("right").innerHTML = "Player B: " + score2++;
+		} // else
 
+	} // if
+
+	// ball on right side of gameBoard
+	if (leftPositionOfBall >= gameBoardWidth - paddleWidth - ballHeight) {
+
+		// if ball hits right paddle, change direction
+		
+		if (topPositionofBall > positionOfPaddle2 && 
+			topPositionofBall < positionOfPaddle2 + paddleHeight) {
+		    leftSpeedOfBall *= -1;
+		
+		} else {
+		  startBall();
+		  document.getElementById("left").innerHTML = "Player A: " + score1++;
+		} // else
+
+	}
+
+	document.getElementById("paddle1").style.top = positionOfPaddle1 + "px";
+	document.getElementById("paddle2").style.top = positionOfPaddle2 + "px";
+	document.getElementById("ball").style.top = topPositionofBall + "px";
+	document.getElementById("ball").style.left = leftPositionOfBall + "px";
+}, 1000/60); // show
 
